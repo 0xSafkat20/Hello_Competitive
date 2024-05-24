5b48da62ba33e4999660d50400f4a45333a8278c
Safkat khan Tafsir <safkatkhan420@gmail.com>
2024-05-24 23:14:01 +0600

    New Problem Solved

--- LigthOJ/Knights on a Chessboard.cpp
+++ LigthOJ/Knights on a Chessboard.cpp
@@ -1,5 +1,6 @@
 //Knights on a Chessboard
 //"Bismillah Rahman Rahim"
+//{
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -13,24 +14,38 @@ using ll = long long;
 
 #define pb    push_back
 #define sz(a) (int)a.size()
+//}
 
-int main()
-{
-    optimize();
-    int t=1;
-    cin>>t;
-    for(int i=1; i<=t; i++)
-    {
-        int m,n;
-        cin>>m>>n;
-        if((m*n)%2==0)
-        {
-            cout<<"Case "<<i<<": "<<(m*n)/2<<endl;
+int max_knights(int m, int n) {
+    if (m == 1 || n == 1) {
+        // If there's only one row or one column, all cells can hold a knight
+        return m * n;
+    } else if (m == 2 || n == 2) {
+        // Special case for 2xN or Mx2 boards
+        if (m == 2) {
+            swap(m, n); // Ensure m >= n
         }
-        else
-        {
-            cout<<"Case "<<i<<": "<<((m*n)/2)+1<<endl;
+        if (n % 4 == 0) {
+            return (n / 4) * 4;
+        } else if (n % 4 == 1) {
+            return (n / 4) * 4 + 2;
+        } else {
+            return (n / 4) * 4 + 4;
         }
+    } else {
+        // General case for larger boards
+        return (m * n + 1) / 2;
+    }
+}
+
+int main() {
+    int T;
+    cin >> T;
+    for (int t = 1; t <= T; ++t) {
+        int m, n;
+        cin >> m >> n;
+        int max_knight_count = max_knights(m, n);
+        cout << "Case " << t << ": " << max_knight_count << endl;
     }
     return 0;
 }
